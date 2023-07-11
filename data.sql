c84c2ced79589760964902ce82d92af8ef680ba6
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-07-11 05:31:46 -0600

    Add data.sql

--- data.sql
+++ data.sql
@@ -1,8 +1,14 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES 
-(1, 'Agumon', '2020-02-03', 0, true, 10.23),
-(2, 'Gabumon', '2018-11-15', 2, true, 8.00),
-(3, 'Pikachu', '2021-01-07', 1, false, 15.04),
-(4, 'Devimon', '2017-05-12', 5, true, 11.00);
+INSERT INTO animals (id, name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES
+(1, 'Agumon', '2020-02-03', 10.23, true, 0),
+(2, 'Gabumon', '2018-11-15', 8.0, true, 2),
+(3, 'Pikachu', '2021-01-07', 15.04, false, 1),
+(4, 'Devimon', '2017-05-12', 11.0, true, 5),
+(5, 'Charmander', '2020-02-08', -11, false, 0),
+(6, 'Plantmon', '2021-11-15', -5.7, true, 2),
+(7, 'Squirtle', '1993-04-02', -12.13, false, 3),
+(8, 'Angemon', '2005-06-12', -45, true, 1),
+(9, 'Boarmon', '2005-06-07', 20.4, true, 7),
+(10, 'Blossom', '1998-10-13', 17, true, 3),
+(11, 'Ditto', '2022-05-14', 22, true, 4);
