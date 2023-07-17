31ba790a300cfa18ed81312946264b72ec71cb2f
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-07-17 04:05:37 -0600

    Add data.sql

--- data.sql
+++ data.sql
@@ -2,6 +2,9 @@
 
 ---------------------------------- Fifth part  ----------------------------------
 
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+INSERT INTO owners (full_name, email) SELECT  'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
 
 ---------------------------------- fourth part  ----------------------------------
 INSERT INTO vets (name, age, date_of_graduation) VALUES
